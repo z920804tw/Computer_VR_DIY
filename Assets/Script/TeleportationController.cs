a62b9ab400e559eb79aed63939dd7d6d329221e7
z920804tw <129352148+z920804tw@users.noreply.github.com>
2023-11-28 12:51:25 +0800

    2023/11/28
    VR的拿取物體和移動功能
    在專案中導入了XR的套件以及預設的手部模型，之後使用到XR的Direct Interactor功能和物件的Grab Interactor來做物件的拿取功能(layer mask設定成Interable才能互動)。
    移動的設定用到了Locomotion跟Teleport Area和Ray Interactor功能來做到移動、旋轉(layer mask設定只有Teleport區域才能移動)

---
+++ Assets/Script/TeleportationController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Allows us to use the Input System to get values for the thumbstick
+using UnityEngine.InputSystem;
+//Allows us to use the Interaction Toolkit to enable and disable our rays
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportationController: MonoBehaviour
+{
+    public InputActionProperty m_teleportModeActivate;
+    public InputActionProperty m_teleprotModeCancel;
+
+    private InputAction teleportModeActivate;
+    private InputAction teleprotModeCancel;
+
+    public XRRayInteractor teleportInteractor;
+
+    private void Start()
+    {
+        teleportModeActivate = m_teleportModeActivate.action;
+        teleprotModeCancel = m_teleprotModeCancel.action;
+
+        EnableAction();
+    }
+
+    private void OnDestroy()
+    {
+        DisableAction();
+    }
+
+    private void Update()
+    {
+        if (CanEnterTeleport())
+        {
+            SetTeleportController(true);
+            return;
+        }
+        if (CanExitTeleport())
+        {
+            SetTeleportController(false);
+            return;
+        }
+    }
+
+    private void SetTeleportController(bool isEnable)
+    {
+        if (teleportInteractor != null)
+        {
+            teleportInteractor.gameObject.SetActive(isEnable);
+        }
+    }
+
+    private bool CanEnterTeleport()
+    {
+        bool isTriggerTeleport = teleportModeActivate != null && teleportModeActivate.triggered;
+        bool isCancelTeleport = teleprotModeCancel != null && teleprotModeCancel.triggered;
+        return isTriggerTeleport && !isCancelTeleport;
+    }
+
+    private bool CanExitTeleport()
+    {        
+        bool isCancelTeleport = teleprotModeCancel != null && teleprotModeCancel.triggered;
+        bool isReleaseTeleport = teleportModeActivate != null && teleportModeActivate.phase == InputActionPhase.Waiting;
+        return isCancelTeleport || isReleaseTeleport;
+    }
+
+    private void EnableAction()
+    {
+        if (teleportModeActivate != null && teleportModeActivate.enabled)
+        {
+            teleportModeActivate.Enable();
+        }
+    }
+
+    private void DisableAction()
+    {
+        if (teleportModeActivate != null && teleportModeActivate.enabled)
+        {
+            teleportModeActivate.Disable();
+        }
+    }
+}
