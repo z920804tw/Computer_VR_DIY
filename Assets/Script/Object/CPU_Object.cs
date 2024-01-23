988548a99d2e2dd0f92155c57bf63f5b635cc465
z920804tw <129352148+z920804tw@users.noreply.github.com>
2024-01-23 12:58:28 +0800

    2024/1/23
    修改了各個電腦零件的ObjectParent，針對每個零件單獨做了cs程式碼，這樣之後再修改特定零件時可以針對每個零件做不同的設定。
    ObjectTransform則也是針對每個零件做函示的新增。
    之後再看要怎麼精簡一些

---
+++ Assets/Script/Object/CPU_Object.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CPU_Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject firstColliderObject;                                  //紀錄第一個碰撞的物件
+    public GameObject[] ObjectsTransform;
+
+    [Header("CPU腳位設定")]
+    public int c_LGA;                                                       //CPU的腳位設定
+
+    public bool isHolding;
+
+
+    [SerializeField] bool isFirstCollider;                                  //判斷是否第一次碰撞
+    Rigidbody rb;
+    bool check;
+
+    void Start()
+    {
+        check = false;
+        isFirstCollider = false;
+        isHolding = false;
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        //判斷碰撞到的物體tag是否與自己的tag一致，如果一致就進到裡面
+        if (other.gameObject.tag == this.gameObject.tag)
+        {
+
+            if (isFirstCollider == false)                                  //判斷是否第一次碰撞
+            {
+                isFirstCollider = true;                                   //設定true，這樣就不會修改到第一次紀錄的值
+                firstColliderObject = other.gameObject;                   //設定第一次碰撞物為碰撞到的物件
+            }
+
+        }
+    }
+
+
+    public void Remove_CPU_setting()
+    {
+        Debug.Log("重製cpu設定");
+        isHolding = false;
+        this.gameObject.transform.SetParent(null);
+        rb.isKinematic = false;
+        if (firstColliderObject != null)
+        {
+
+            firstColliderObject.GetComponent<Object_Transform>().hasPlace = false;
+            firstColliderObject = null;
+            isFirstCollider = false;
+        }
+        if (check == true)
+        {
+            if (ObjectsTransform != null)                                      //看放置座標陣列裡有沒有值，如果有才會執行
+            {
+                foreach (GameObject obj in ObjectsTransform)             //用foreach來把該陣列裡面的所有物件的Outline都關閉
+                {
+
+                    if (obj.GetComponent<Outline>() != null)               //會先檢查這個物件有沒有Outline這個Component，如果有才會把他關閉，否則就什麼都不做
+                    {
+                        obj.GetComponent<Outline>().enabled = false;
+                    }
+                }
+            }
+            check = false;
+        }
+
+    }
+
+    public void showCpuOutline()
+    {
+        if (check == false)
+        {
+
+            ObjectsTransform = GameObject.FindGameObjectsWithTag(this.gameObject.tag);                //每次抓取特定物件就會去抓跟這個物件tag一致的物件
+            if (ObjectsTransform != null)
+            {
+                foreach (GameObject obj in ObjectsTransform)
+                {
+                    if (obj.GetComponent<Outline>() != null && obj.GetComponent<Object_Transform>() != null)               //會先檢查這個物件有沒有Outline這個Component，如果有才會把他關閉，否則就什麼都不做
+                    {
+                        if (obj.GetComponent<Object_Transform>().m_LGA == c_LGA)
+                        {
+
+                            obj.GetComponent<Outline>().OutlineColor = new Color(255f / 255, 208f / 255, 0f, 255f / 255);
+                        }
+                        else
+                        {
+                            obj.GetComponent<Outline>().OutlineColor = Color.red;
+
+                        }
+                    }
+                    obj.GetComponent<Outline>().enabled = true;
+                }
+
+            }
+            isHolding=true;
+            check = true;
+
+
+        }
+
+    }
+}
---
+++ Assets/Script/Object/GraphicsCard_Object.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicsCard_Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject firstColliderObject;                                  //紀錄第一個碰撞的物件
+    public GameObject[] ObjectsTransform;
+
+
+    public bool isHolding;
+
+
+    [SerializeField] bool isFirstCollider;                                  //判斷是否第一次碰撞
+    Rigidbody rb;
+    bool check;
+    void Start()
+    {
+        check = false;
+        isFirstCollider = false;
+        isHolding = false;
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        //判斷碰撞到的物體tag是否與自己的tag一致，如果一致就進到裡面
+        if (other.gameObject.tag == this.gameObject.tag)
+        {
+
+            if (isFirstCollider == false)                                  //判斷是否第一次碰撞
+            {
+                isFirstCollider = true;                                   //設定true，這樣就不會修改到第一次紀錄的值
+                firstColliderObject = other.gameObject;                   //設定第一次碰撞物為碰撞到的物件
+            }
+
+        }
+    }
+
+    public void Remove_GraphicsCard_Setting()
+    {
+
+        Debug.Log("重製顯示卡設定");
+        isHolding = false;
+        this.gameObject.transform.SetParent(null);
+        rb.isKinematic = false;
+        if (firstColliderObject != null)
+        {
+
+            firstColliderObject.GetComponent<Object_Transform>().hasPlace = false;
+            firstColliderObject = null;
+            isFirstCollider = false;
+        }
+        if (check == true)
+        {
+            if (ObjectsTransform != null)                                      //看放置座標陣列裡有沒有值，如果有才會執行
+            {
+                foreach (GameObject obj in ObjectsTransform)             //用foreach來把該陣列裡面的所有物件的Outline都關閉
+                {
+
+                    if (obj.GetComponent<Outline>() != null)               //會先檢查這個物件有沒有Outline這個Component，如果有才會把他關閉，否則就什麼都不做
+                    {
+                        obj.GetComponent<Outline>().enabled = false;
+                    }
+                }
+            }
+            check = false;
+        }
+    }
+    public void showGraphicsCardOutline()
+    {
+        if (check == false)
+        {
+            ObjectsTransform = GameObject.FindGameObjectsWithTag(this.gameObject.tag);                //每次抓取特定物件就會去抓跟這個物件tag一致的物件
+            if (ObjectsTransform != null)
+            {
+                foreach (GameObject obj in ObjectsTransform)
+                {
+                    obj.GetComponent<Outline>().enabled = true;
+                }
+            }
+            isHolding = true;
+            check = true;
+        }
+    }
+}
