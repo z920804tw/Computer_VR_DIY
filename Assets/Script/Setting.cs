54eb7095ef20a215e7f7bead43a966fc6ed19011
z920804tw <129352148+z920804tw@users.noreply.github.com>
2024-02-06 11:44:14 +0800

    2024/2/6
    設定UI的基本功能
    引導語音音量調整功能以及一些UI外觀和按鈕設定。

--- Assets/Script/Setting.cs
+++ Assets/Script/Setting.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR;
 using UnityEngine.InputSystem;
-using Unity.Mathematics;
+using UnityEngine.EventSystems;
+using TMPro;
+using UnityEngine.UI;
 public class Setting : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("關聯物件設定")]
     public GameObject SettingMenu;
+    public GameObject[] Menu;
     public GameObject SettingMenuPos;
     public Transform camR;
-    public InputActionReference testAction;
+    [Header("音量設定")]
+    public Slider guideVolumeSlider;
+    public AudioClip[] audioClips;
+    public AudioSource audioSource;
+
+    [Header("VR按鈕設定")]
+    public InputActionReference OpenMenu;
+    
+
+    static float guideVolume=0.5f;
+
     InputAction Action;
 
     bool settingMenuOpen;
@@ -20,8 +34,10 @@ public class Setting : MonoBehaviour
 
     void Start()
     {
+        guideVolumeSlider.value=guideVolume;
+        audioSource.volume=guideVolume;
         settingMenuOpen = false;
-        Action = testAction;
+        Action = OpenMenu;
         Action.performed += Test;
     }
 
@@ -44,7 +60,38 @@ public class Setting : MonoBehaviour
 
     }
 
+    public  void SwitchMenu(){
+        string currentBtn=EventSystem.current.currentSelectedGameObject.GetComponentInChildren<TextMeshProUGUI>().text;
+        closeAllSettingMenu();
+        switch(currentBtn){
+            case "音量設定":
+                Menu[1].SetActive(true);
+            break;
+            case "返回設定選單":
+                Menu[0].SetActive(true);
+            break;
+            default:
+            break;
+        }
+    }
+    public void closeMenu(){
+        closeAllSettingMenu();
+        Menu[0].SetActive(true);
+        SettingMenu.SetActive(false);
+        settingMenuOpen=false;
 
+    }
+    public void closeAllSettingMenu(){
+        foreach( GameObject i in Menu){
+            i.SetActive(false);
+        }
+    }
+    public void testGuideVolume(){
+        guideVolume=guideVolumeSlider.value;
+        audioSource.volume=guideVolume;
+        int rnd=Random.Range(0,9);
+        audioSource.PlayOneShot(audioClips[rnd]);
+    }
 
 
 }
