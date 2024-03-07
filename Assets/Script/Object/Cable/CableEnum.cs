a7243962c098868e01bb9d16246e6d28dc9d7cda
z920804tw <129352148+z920804tw@users.noreply.github.com>
2024-03-07 12:34:14 +0800

    2024/3/7
    CPU和主機板供電線的接線功能，包含主機板的那端跟Power的那端，遇到的問題是關於整線的部分，電線會穿過主機殼，這個問題之後看看有甚麼方法可以修正

--- Assets/Script/Object/Cable/CableEnum.cs
+++ Assets/Script/Object/Cable/CableEnum.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CableType { None=0,CPU,GraphicsCard,MotherBoardPower,Power,Hardware,Fan};
+public enum CableType { None=0,CPU,GraphicsCard,MotherBoardPower,Power,Hardware,Fan,PCIE};
 
 public enum CableDirection {None=0,Start,End};
 
