249429885b61444739387745b64cde90736a65b4
z920804tw <129352148+z920804tw@users.noreply.github.com>
2024-05-22 10:50:56 +0800

    2024/5/22
    123

--- Assets/Script/Object/CPU_Fan_Object.cs
+++ Assets/Script/Object/CPU_Fan_Object.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using Unity.VisualScripting.FullSerializer;
-using UnityEditor.Animations;
+
 using UnityEngine;
 
 public class CPU_Fan_Object : MonoBehaviour
