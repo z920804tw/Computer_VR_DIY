e5180937acef96df4c0a7c5258c39b32b1d1ef2e
z920804tw <129352148+z920804tw@users.noreply.github.com>
2024-05-28 11:54:06 +0800

    2024/5/28
    將完整電腦組裝的關卡完成,還沒有實際的戴VR測試,之後會測試看看有沒有問題需要修正,然後也替此關卡烘培了場景光源

--- Assets/Script/Tutorial_Level/ComputerDIY_Level/ComputerDIY_Level_Status.cs
+++ Assets/Script/Tutorial_Level/ComputerDIY_Level/ComputerDIY_Level_Status.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ComputerDIY_Level_Status : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("雜項")]
     [SerializeField] int status;
     [SerializeField] Level_Select l;
     [SerializeField] GameObject[] MenuPanels;
+    [SerializeField] GameObject CpuThermal;
+
+    int currentTransformIndex = 0;
+
     [Header("CPU物件設定")]
     [SerializeField] Object_Transform CPU_Transform;
     [SerializeField] Object_Transform CPU_Fan_Transform;
@@ -21,7 +28,7 @@ public class ComputerDIY_Level_Status : MonoBehaviour
     [Header("顯示卡物件設定")]
     [SerializeField] Object_Transform GraphicsCard_Transform;
     [SerializeField] Object_Transform[] GraphiscCard_Cable_Transform;
-    
+
     [Space]
     [Header("主機板物件設定")]
     [SerializeField] Object_Transform MotherBoard_Transform;
@@ -51,11 +58,186 @@ public class ComputerDIY_Level_Status : MonoBehaviour
         {
             audioSource.volume = Menu.guideVolume;
         }
+        currentTransformIndex = -1;
+        status = 0;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (status)
+        {
+            case 0:
+                MenuPanels[0].SetActive(true);
+                break;
+            case 1:
+                MenuPanels[1].SetActive(true);
+                if (CPU_Transform.hasPlace == true)
+                {
+                    NextStatus();
+                }
+                break;
+            case 2:
+                MenuPanels[2].SetActive(true);
+                if (CpuThermal.activeSelf == true)
+                {
+                    if (CPU_Fan_Transform.hasPlace && CPU_Fan_Cable_transform.hasPlace)
+                    {
+                        NextStatus();
+                    }
+                }
+                break;
+            case 3:
+                MenuPanels[3].SetActive(true);
+                if (currentTransformIndex == -1)
+                {
+                    foreach (Object_Transform i in Memory_Transform)
+                    {
+                        if (i.hasPlace == true)
+                        {
+                            currentTransformIndex = Array.IndexOf(Memory_Transform, i);
+                        }
+                    }
+                }
+                else if (currentTransformIndex != -1)
+                {
+                    switch (currentTransformIndex)
+                    {
+                        case 0:
+                        case 2:
+                            Memory_Transform[1].gameObject.SetActive(false);
+                            Memory_Transform[3].gameObject.SetActive(false);
+                            checkIndex(currentTransformIndex);
+                            break;
+                        case 1:
+                        case 3:
+                            Memory_Transform[0].gameObject.SetActive(false);
+                            Memory_Transform[2].gameObject.SetActive(false);
+                            checkIndex(currentTransformIndex);
+                            break;
+                    }
+                }
+                break;
+            case 4:
+                MenuPanels[4].SetActive(true);
+                if (M2_SSD_Transform.hasPlace && MB_Screw__transform[8].hasPlace)
+                {
+                    NextStatus();
+                }
+                break;
+            case 5:
+                MenuPanels[5].SetActive(true);
+                if (MotherBoard_Transform.hasPlace == true)
+                {
+                    if (MB_Screw__transform[0].hasPlace && MB_Screw__transform[1].hasPlace && MB_Screw__transform[2].hasPlace &&
+                        MB_Screw__transform[3].hasPlace && MB_Screw__transform[4].hasPlace && MB_Screw__transform[5].hasPlace &&
+                        MB_Screw__transform[6].hasPlace && MB_Screw__transform[7].hasPlace)
+                    {
+                        NextStatus();
+                    }
+                }
+                break;
+            case 6:
+                MenuPanels[6].SetActive(true);
+                if (Power_Transform.hasPlace == true)
+                {
+                    if (PW_Screw__transform[0].hasPlace && PW_Screw__transform[1].hasPlace &&
+                        PW_Screw__transform[2].hasPlace && PW_Screw__transform[3].hasPlace)
+                    {
+                        NextStatus();
+                    }
+                }
+                break;
+            case 7:
+                MenuPanels[7].SetActive(true);
+                if (GraphicsCard_Transform.hasPlace == true)
+                {
+                    if (GraphiscCard_Cable_Transform[0].hasPlace && GraphiscCard_Cable_Transform[1].hasPlace)
+                    {
+                        NextStatus();
+                    }
+                }
+                break;
+            case 8:
+                MenuPanels[8].SetActive(true);
+                if (HHD_Transform.hasPlace == true)
+                {
+                    if (HardDriver_Cable_Transform[0].hasPlace && HardDriver_Cable_Transform[1].hasPlace &&
+                        HardDriver_Cable_Transform[2].hasPlace && HardDriver_Cable_Transform[3].hasPlace)
+                    {
+                        NextStatus();
+                    }
+                }
+                break;
+            case 9:
+                MenuPanels[9].SetActive(true);
+                if (Cable_Transform[0].hasPlace && Cable_Transform[1].hasPlace &&
+                    Cable_Transform[2].hasPlace && Cable_Transform[3].hasPlace)
+                {
+                    NextStatus();
+                }
+                break;
 
+            case 10:
+                MenuPanels[10].SetActive(true);
+                break;
+            default:
+                break;
+
+
+        }
+
+
+
+
+    }
+
+    void checkIndex(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                if (Memory_Transform[2].hasPlace == true)
+                {
+                    NextStatus();
+                }
+                break;
+
+            case 1:
+                if (Memory_Transform[3].hasPlace == true)
+                {
+                    NextStatus();
+                }
+                break;
+
+            case 2:
+                if (Memory_Transform[0].hasPlace == true)
+                {
+                    NextStatus();
+                }
+                break;
+
+            case 3:
+                if (Memory_Transform[1].hasPlace == true)
+                {
+                    NextStatus();
+                }
+                break;
+        }
+    }
+
+    public void NextStatus()
+    {
+        if (l != null)
+        {
+            l.closeAllUI(MenuPanels);
+        }
+        else
+        {
+            Debug.Log("找不到L");
+        }
+        status++;
     }
 }
